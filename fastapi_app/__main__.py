d8cea0e675227e14a51954e11f14ef398b890409
roman mac <barabum@duck.com>
2023-12-09 16:14:51 +0300

    :recycle: refactor: move FastAPI instance creation to top-level and update uvicorn run call

--- fastapi_app/__main__.py
+++ fastapi_app/__main__.py
@@ -5,15 +5,16 @@ from loguru import logger
 from fastapi_app.routers import router as main_router
 from fastapi_app.tools.logs import configure_logger, get_uvicorn_log_config
 
+app = FastAPI()
+
 
 def main():
     configure_logger()
     logger.info("Starting...")
 
-    app = FastAPI()
     app.include_router(main_router)
 
-    uvicorn.run(app, log_config=get_uvicorn_log_config())
+    uvicorn.run("app.__main__:app", log_config=get_uvicorn_log_config())
 
 
 if __name__ == "__main__":
