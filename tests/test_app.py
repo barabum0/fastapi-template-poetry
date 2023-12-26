b8f3bd1f6d1775047e43b87d762c931e003ae1a9
roman mac <barabum@duck.com>
2023-12-26 21:16:32 +0300

    ✨ test(app): use pytest fixture for test client in test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,25 @@
 # mypy: ignore_errors
+import pytest
 from fastapi.testclient import TestClient
 from fastapi_app.routers import router as main_router
 from fastapi_app.__main__ import app
 
 
-def test_hello_world_1():
+@pytest.fixture()
+def client() -> TestClient:
     app.include_router(main_router)
     client = TestClient(app)
 
+    return client
+
+
+def test_hello_world_1(client: TestClient):
     response = client.post("/api/hi", json={"name": "Roman"})
     assert response.status_code == 200
     assert response.json() == {"greeting": "Hello, Roman!"}
 
 
-def test_hello_world_2():
-    app.include_router(main_router)
-    client = TestClient(app)
-
+def test_hello_world_2(client: TestClient):
     response = client.post("/api/hi", json={"name": "Alex"})
     assert response.status_code == 200
     assert response.json() == {"greeting": "Hello, Alex!"}
